06eeca05bf7603c84f86ee999467b5c4803fd4cd
Simone Carletti <weppos@weppos.net>
2020-05-10 15:03:18 +0200

    Cleanup copyright

--- src/dnsimple/Properties/AssemblyInfo.cs
+++ src/dnsimple/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DNSimple")]
 [assembly: AssemblyProduct("dnsimple-csharp")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright (c) DNSimple Corporation")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
