5d1b18b2e1222ced4688320394e2b9bbb22b0e25
Enrique Comba Riepenhausen <enrique@ecomba.pro>
2020-06-18 17:18:14 +0100

    Release 0.9.0

--- src/dnsimple/Properties/AssemblyInfo.cs
+++ src/dnsimple/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.6")]
-[assembly: AssemblyFileVersion("0.8.6")]
+[assembly: AssemblyVersion("0.9.0")]
+[assembly: AssemblyFileVersion("0.9.0")]
 // This version is the one used for the user agent
-[assembly: AssemblyInformationalVersion("0.8.6-alpha")]
+[assembly: AssemblyInformationalVersion("0.9.0")]
