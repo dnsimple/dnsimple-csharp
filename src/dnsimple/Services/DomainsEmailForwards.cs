32649a28a730076b5d025a711549c93342d26a22
Stella Lok <stellalok@gmail.com>
2025-08-18 11:56:47 +0800

    Add active attribute

--- src/dnsimple/Services/DomainsEmailForwards.cs
+++ src/dnsimple/Services/DomainsEmailForwards.cs
@@ -128,6 +128,8 @@ namespace dnsimple.Services
             }
         }
 
+        public bool Active { get; set; }
+
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
