5210236ce1bdc290bf4cb933014eb98bcbeb16cc
Simone Carletti <5387+weppos@users.noreply.github.com>
2026-01-13 17:53:34 +0100

    Removed deprecated GetWhoisPrivacy and RenewWhoisPrivacy (#208)
    Removes the deprecated `GetWhoisPrivacy` and `RenewWhoisPrivacy` endpoints and all associated code.
    
    See https://github.com/dnsimple/dnsimple-developer/pull/919

--- src/dnsimple/Services/RegistrarWhoisPrivacy.cs
+++ src/dnsimple/Services/RegistrarWhoisPrivacy.cs
@@ -14,20 +14,6 @@ namespace dnsimple.Services
     public partial class RegistrarService
     {
         /// <summary>
-        /// Gets the whois privacy status for an existing domain.
-        /// </summary>
-        /// <param name="accountId">The account ID</param>
-        /// <param name="domain">The domain name</param>
-        /// <returns>The WHOIS privacy for the domain</returns>
-        /// <see>https://developer.dnsimple.com/v2/registrar/whois-privacy/#getWhoisPrivacy</see>
-        public SimpleResponse<WhoisPrivacy> GetWhoisPrivacy(long accountId, string domain)
-        {
-            var builder = BuildRequestForPath(WhoisPrivacyPath(accountId, domain)); 
-
-            return new SimpleResponse<WhoisPrivacy>(Execute(builder.Request));
-        }
-
-        /// <summary>
         /// Enables the WHOIS privacy for the domain.
         /// </summary>
         /// <remarks>
@@ -64,21 +50,6 @@ namespace dnsimple.Services
 
             return new SimpleResponse<WhoisPrivacy>(Execute(builder.Request));
         }
-
-        /// <summary>
-        /// Renews the WHOIS privacy for the domain.
-        /// </summary>
-        /// <param name="accountId">The account ID</param>
-        /// <param name="domain">The domain name</param>
-        /// <returns>A whois renewal response with the renewal information</returns>
-        /// <see>https://developer.dnsimple.com/v2/registrar/whois-privacy/#renewWhoisPrivacy</see>
-        public SimpleResponse<WhoisPrivacyRenewal> RenewWhoisPrivacy(long accountId, string domain)
-        {
-            var builder = BuildRequestForPath(WhoisRenewalPath(accountId, domain));
-            builder.Method(Method.POST);
-
-            return new SimpleResponse<WhoisPrivacyRenewal>(Execute(builder.Request));
-        }
     }
 
     /// <summary>
@@ -89,22 +60,6 @@ namespace dnsimple.Services
     {
         public long Id { get; set; }
         public long DomainId { get; set; }
-        public DateTime? ExpiresOn { get; set; }
-        public bool? Enabled { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-    }
-
-    /// <summary>
-    /// Represents the WHOIS privacy renewal data for a domain.
-    /// </summary>
-    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
-    public struct WhoisPrivacyRenewal
-    {
-        public long Id { get; set; }
-        public long DomainId { get; set; }
-        public long WhoisPrivacyId { get; set; }
-        public string State { get; set; }
         public DateTime? ExpiresOn { get; set; }
         public bool? Enabled { get; set; }
         public DateTime CreatedAt { get; set; }
